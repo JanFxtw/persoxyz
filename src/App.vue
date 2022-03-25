81d55f47827b993e572138d818f3442cf80057e4
Jan Wieczorek <info@kampagne-online.de>
2022-03-25 16:22:40 +0100

    adds basic structure & normalize

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <Dashboard />
+    <Footer />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import Header from './components/app/Header';
+import Dashboard from './components/app/Dashboard';
+import Footer from './components/app/Footer';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    Header,
+    Dashboard,
+    Footer,
   },
 };
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
